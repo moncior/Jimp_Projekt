acde155d6f05c5a9e9c48094117c391bca996f5d
moncior <olioziemblewski@gmail.com>
2026-02-25 12:07:28 +0100

    Stworzenie struktur grafu

---
+++ graf.h
@@ -0,0 +1,14 @@
+#ifndef GRAF_H
+#define GRAF_H
+
+typedef struct {
+    double x;
+    double y;
+    double w;
+}punkty;
+
+typedef struct {
+    double poczatek;
+    double koniec;
+    double wartosc;
+}krawedzie;
