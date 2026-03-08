741e249175126873184fec0929a63fbdc6655093
moncior <olioziemblewski@gmail.com>
2026-03-08 12:39:39 +0100

    plik.h

--- plik.h
+++ plik.h
@@ -2,4 +2,10 @@
 #define PLIK_H
 #include "graf.h"
 
+void wczytywanie(graf*g,const char *nazwa_pliku);
+
+void wypisywanie_tekstowo(graf*g,const char*nazwa_pliku);
+
+void wypisywanie_binarnie(graf*g,const char*nazwa_pliku);
+
 #endif 
