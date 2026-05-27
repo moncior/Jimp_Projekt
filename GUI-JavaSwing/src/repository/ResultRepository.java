a694d6d013fb3301086ebc23f42e6a4a75ec9e47
343306 <01206747@pw.edu.pl>
2026-05-27 22:45:50 +0200

    Add edges handling in ResultRepository

--- GUI-JavaSwing/src/repository/ResultRepository.java
+++ GUI-JavaSwing/src/repository/ResultRepository.java
@@ -9,6 +9,7 @@ public class ResultRepository {
 
     private Point2D.Double[] points;
     private int[] ids;
+    private Edge[] edges;
 
     public void readCoordinates(File coord){
 
@@ -38,9 +39,37 @@ public class ResultRepository {
         points = pointsList.toArray(new Point2D.Double[0]);
 
         ids = new int[idsList.size()];
+
         for(int i = 0; i < idsList.size(); i++){
             ids[i] = idsList.get(i);
         }
+    }
+
+    public void readEdges(File edgeFile){
+
+        List<Edge> edgesList = new ArrayList<>();
+
+        try(BufferedReader in = new BufferedReader(new FileReader(edgeFile))){
+
+            String line;
+
+            while((line = in.readLine()) != null){
+
+                String[] data = line.split(" ");
+
+                int id = Integer.parseInt(data[0]);
+                int from = Integer.parseInt(data[1]);
+                int to = Integer.parseInt(data[2]);
+                double length = Double.parseDouble(data[3]);
+
+                edgesList.add(new Edge(id, from, to, length));
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        edges = edgesList.toArray(new Edge[0]);
     }
 
     public Point2D.Double[] getPoints(){
@@ -49,5 +78,9 @@ public class ResultRepository {
 
     public int[] getIds(){
         return ids;
+    }
+
+    public Edge[] getEdges(){
+        return edges;
     }
 }
