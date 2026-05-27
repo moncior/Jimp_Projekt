85b47949dc504281cee98d2eccda97044921155f
343306 <01206747@pw.edu.pl>
2026-05-27 22:45:02 +0200

    Add edges to DrawGraph constructor and rendering

--- GUI-JavaSwing/src/gui/DrawGraph.java
+++ GUI-JavaSwing/src/gui/DrawGraph.java
@@ -3,15 +3,18 @@ package gui;
 import java.awt.*;
 import java.awt.geom.Point2D;
 import javax.swing.JPanel;
+import repository.Edge;
 
 public class DrawGraph extends JPanel {
 
     private Point2D.Double[] points;
     private int[] ids;
+    private Edge[] edges;
 
-    public DrawGraph(Point2D.Double[] pts, int[] ids) {
+    public DrawGraph(Point2D.Double[] pts, int[] ids, Edge[] edges) {
         this.points = pts;
         this.ids = ids;
+        this.edges = edges;
     }
 
     @Override
@@ -28,34 +31,52 @@ public class DrawGraph extends JPanel {
 
         double scale = 20;
 
+        // AXIS
         g2.setColor(Color.BLACK);
         g2.drawLine(0, centerY, getWidth(), centerY);
         g2.drawLine(centerX, 0, centerX, getHeight());
 
-        g2.setColor(Color.RED);
+        // KRAWEDZIE
+        for (Edge edge : edges) {
 
-        for (int i = 0; i < points.length; i++) {
+            int fromIndex = -1;
+            int toIndex = -1;
 
-            int next = (i + 1) % points.length;
+            // znalezienie indeksow punktow
+            for (int i = 0; i < ids.length; i++) {
 
-            int x1 = centerX + (int)(points[i].x * scale);
-            int y1 = centerY - (int)(points[i].y * scale);
+                if (ids[i] == edge.from) {
+                    fromIndex = i;
+                }
 
-            int x2 = centerX + (int)(points[next].x * scale);
-            int y2 = centerY - (int)(points[next].y * scale);
+                if (ids[i] == edge.to) {
+                    toIndex = i;
+                }
+            }
 
+            if (fromIndex == -1 || toIndex == -1) {
+                continue;
+            }
+
+            Point2D.Double p1 = points[fromIndex];
+            Point2D.Double p2 = points[toIndex];
+
+            int x1 = centerX + (int)(p1.x * scale);
+            int y1 = centerY - (int)(p1.y * scale);
+
+            int x2 = centerX + (int)(p2.x * scale);
+            int y2 = centerY - (int)(p2.y * scale);
+
+            // linia
             g2.setColor(Color.RED);
             g2.drawLine(x1, y1, x2, y2);
 
-            double length = Math.sqrt(
-                    Math.pow(points[next].x - points[i].x, 2) +
-                    Math.pow(points[next].y - points[i].y, 2)
-            );
-
+            // srodek linii
             int midX = (x1 + x2) / 2;
             int midY = (y1 + y2) / 2;
 
-            String lengthText = String.format("%.2f", length);
+            // tekst dlugosci
+            String lengthText = String.format("%.2f", edge.length);
 
             double angle = Math.atan2(y2 - y1, x2 - x1);
 
@@ -67,11 +88,12 @@ public class DrawGraph extends JPanel {
 
             g2.translate(midX, midY);
             g2.rotate(angle);
-            g2.drawString(lengthText, -10, -6);
+            g2.drawString(lengthText, -10, -5);
             g2.rotate(-angle);
             g2.translate(-midX, -midY);
         }
 
+        // PUNKTY
         g2.setColor(Color.BLUE);
 
         for (int i = 0; i < points.length; i++) {
