f48b4314beead1d87cc5c299b1d953e375f6f0b1
343306 <01206747@pw.edu.pl>
2026-05-27 22:05:01 +0200

    Refactor ResultRepository for coordinate management
    Refactored ResultRepository to manage coordinates and IDs. Added methods to read coordinates from a file and retrieve points and IDs.

--- GUI-JavaSwing/src/repository/ResultRepository.java
+++ GUI-JavaSwing/src/repository/ResultRepository.java
@@ -1,54 +1,53 @@
 package repository;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
+import java.awt.geom.Point2D;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
-
+import java.util.List;
 
 public class ResultRepository {
-    private HashMap<Integer, Coordinates> position;
-    private ArrayList<Adjacency> graph;
 
-    public ResultRepository(){
-        position = new HashMap<>();
-        graph = new ArrayList<>();
-    }
-    public void readGraph(File adjacency){
-        try(BufferedReader in = new BufferedReader(new FileReader(adjacency))){
-            String line;
-            while((line = in.readLine()) != null){
-                String[] data = line.split(" ");
-                int from = Integer.parseInt(data[0]);
-                int to = Integer.parseInt(data[1]);
-                double weight = Double.parseDouble(data[2]);
-                graph.add(new Adjacency(from, to, weight));
-            }
-        }
-        catch(IOException e){
-            //wyswietlic blad w gui
-        }
+    private Point2D.Double[] points;
+    private int[] ids;
 
-
-
-    }
     public void readCoordinates(File coord){
+
+        List<Point2D.Double> pointsList = new ArrayList<>();
+        List<Integer> idsList = new ArrayList<>();
+
         try(BufferedReader in = new BufferedReader(new FileReader(coord))){
+
             String line;
+
             while((line = in.readLine()) != null){
+
                 String[] data = line.split(" ");
+
                 int id = Integer.parseInt(data[0]);
                 double x = Double.parseDouble(data[1]);
                 double y = Double.parseDouble(data[2]);
-                position.put(id, new Coordinates(x, y));
+
+                idsList.add(id);
+                pointsList.add(new Point2D.Double(x, y));
             }
         }
         catch(IOException e){
-            //wyswietlic blad w gui
+            e.printStackTrace();
+        }
+
+        points = pointsList.toArray(new Point2D.Double[0]);
+
+        ids = new int[idsList.size()];
+        for(int i = 0; i < idsList.size(); i++){
+            ids[i] = idsList.get(i);
         }
     }
-}
 
+    public Point2D.Double[] getPoints(){
+        return points;
+    }
+
+    public int[] getIds(){
+        return ids;
+    }
+}
